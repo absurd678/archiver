44d9e61deb028bc3cbc017a8cf15da357a6a3f51
absurd678 <artemykozhevnik@gmail.com>
2024-09-24 15:08:57 +0300

    Saving the folder hierarchy instead of copying real paths. Extraction default and user options.

--- archiver.c
+++ archiver.c
@@ -5,101 +5,158 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
- 
 //-----------------CONSTANTS-----------------
-const int BUFFSIZE1024 = 1024;  // Buffer size for reading files
+const int BUFFSIZE1024 = 1024; // Buffer size for reading files
 const int buffSize256 = 256;
-const int pATH_MAX = 4096;  // Maximum path length
+const int pATH_MAX = 4096; // Maximum path length
+
+// TODO: Replace hardcoded paths with environment variables
+const char* DEFAULT_PATH = "/home/artem/os_labs/archiver"; // Default path for archive file
+
+// Error codes
+const int ERR_OPN_DIR = 1;
+const int ERR_OPN_FILE = 2;
+const int ERR_WRT_ARCH = 3;
+const int ERR_RD_ARCH = 4;
+const int ERR_EXTR_ARCH = 5;
+const int ERR_CRT_DIR = 6;
+const int ERR_EXTR = 7;
+const int ERR_PTH_ARCH = 8;
+
 
 
 //-----------------PROTOTYPES-----------------
-int archiveFile(char *dir, FILE* file_out); // Archive file
-int processDir(char *dir, FILE* file_out);  // 
-int createNewDirectory(char *curr_dir, char *base_path); // Create new directory
-int processArchive(FILE* archiveFile);  // Process archive file
-void PrintErr(int errorCode); // Print error code
-
+int archiveFile(char *dir, size_t root_size, FILE *file_out); // Archive file
+int processDir(char *dir, FILE *file_out, size_t root_size);  //
+int createNewDirectory(char *curr_dir, char *base_path);      // Create new directory
+int processArchive(FILE *archiveFile, char *top_dir_path);    // Process archive file
+void PrintErr(int errorCode);                                 // Print error code
 
 //-----------------VARIABLES-----------------
-FILE* archiver;  // Archive file
-char topdir[BUFFSIZE1024];  // Current directory
-char archiveFileName[buffSize256];  // Archive file name
-int opt;  // Action (1 - archive, 2 - extract)
-int errCode = 0;  // Error code
-
+FILE *archiver;                    // Archive file
+char topdir[BUFFSIZE1024];         // Current directory
+char archiveFileName[buffSize256]; // Archive file name
+int opt;                           // Action (1 - archive, 2 - extract)
+int errCode = 0;                   // Error code
+char res[buffSize256];             // Response from user
+char extractName[buffSize256];     // Extract directory name
 
 //-----------------MAIN FUNCTION-----------------
 
-int main() {
+int main()
+{
 
     printf("Choose the action:\n1. Archive\n2. Extract\n/>");
     scanf("%d", &opt);
 
-    switch (opt) {
+    switch (opt)
+    {
 
-        case 1:  // Archive
+    case 1: // Archive
 
-            printf("Write the realpath of the directory to archive:\n/>");
-            scanf("%s", topdir);
-            printf("Write the name of the archive:\n/>");
+        printf("Write the realpath of the directory to archive:\n/>");
+        scanf("%s", topdir);
+
+        printf("Put the archive by default? (Y/N)\n/>");
+        scanf("%s", res);
+        if (res[0] == 'N' || res[0] == 'n')
+        {
+            printf("Enter the realpath of the directory to archive:\n/>");
             scanf("%s", archiveFileName);
-
-            archiver = fopen(archiveFileName, "ab+");
-            
-            if ((errCode = processDir(topdir, archiver))!=0){
-                PrintErr(errCode);
-                fclose(archiver);
+            if (fopen(archiveFileName, "r") == NULL) // error if the directory does not exist
+            {
+                PrintErr(ERR_OPN_DIR);
                 break;
             }
-            printf("Archive created successfully with path:\n");
-            printf("%s\n", realpath(archiveFileName, NULL));
+        }
+        printf("Write the name of the archive:\n/>");
+        scanf("%s", res);
+
+        strcat(archiveFileName, res);
+        archiver = fopen(archiveFileName, "ab+");
+
+        // Archiving - not correct, we need just the names of the files!
+        if ((errCode = processDir(topdir, archiver, strlen(topdir))) != 0)
+        {
+            PrintErr(errCode);
             fclose(archiver);
             break;
+        }
+        printf("Archive created successfully with path:\n");
+        printf("%s\n", realpath(archiveFileName, NULL));
+        fclose(archiver);
+        break;
 
-        case 2:  // Extract
+    case 2: // Extract
 
-            // TODO: add the option to choose the extraction directory
-            printf("Write the realpath of the archive to extract:\n/>");
-            scanf("%s", archiveFileName);
-            if ((archiver = fopen(archiveFileName, "r"))==NULL){
-                PrintErr(8);
+        // Choosing the archive file
+        printf("Write the realpath of the archive to extract:\n/>");
+        scanf("%s", archiveFileName);
+        if ((archiver = fopen(archiveFileName, "r")) == NULL)
+        {
+            PrintErr(ERR_PTH_ARCH);
+            break;
+        }
+
+        // Choosing the extraction destination
+        printf("Put the archive by default? (Y/N)\n/>");
+        scanf("%s", res);
+        if (res[0] == 'N' || res[0] == 'n')
+        {
+            printf("Enter the realpath of the directory for extract:\n/>");
+            scanf("%s", res);
+            if (fopen(res, "r") == NULL) // error if the directory does not exist
+            {
+                PrintErr(ERR_OPN_DIR);
                 break;
             }
-            processArchive(archiver);
-            printf("Extracted successfully with path:\n");
-            printf("%s\n", realpath(archiveFileName, NULL));
-            fclose(archiver);
-            break;
+        } else {
+            strcpy(res, DEFAULT_PATH);
+        }
+        // Ask for the extract directory name
+        printf("Write the extract directory name:\n/>");
+        scanf("%s", extractName);
+        strcat(res, "/");
+        strcat(res, extractName);
 
-        default:
+        // Extracting
+        processArchive(archiver, res);
+        printf("Extracted successfully with path:\n");
+        printf("%s\n", realpath(res, NULL));
+        fclose(archiver);
+        break;
 
-            printf("Invalid option. Exiting.\n");
-            exit(1);
+    default:
 
+        printf("Invalid option. Exiting.\n");
+        exit(1);
     }
-    
+
     exit(0);
 }
 
-
 //-----------------IMPLEMENTATION OF FUNCTIONS-----------------
 
-int archiveFile(char *dir, FILE* file_out){
-    
+int archiveFile(char *dir, size_t root_size, FILE *file_out)
+{
+
     int errCode = 0;
 
-    FILE* file_to_archive = fopen(dir, "r");
-    
+    FILE *file_to_archive = fopen(dir, "r");
 
-    if(!file_to_archive){
-        return 2;
+    if (!file_to_archive)
+    {
+        return ERR_OPN_FILE;
     }
 
-    
     // Put the path and size of the file
     char full_path[pATH_MAX];
     realpath(dir, full_path);
-    fputs(full_path, file_out);
+
+    // full_path = full_path - top_dir_path
+    char *new_path = full_path + root_size;
+
+    fputs(new_path, file_out);
     fputs(" | ", file_out);
 
     fseek(file_to_archive, 0, SEEK_END);
@@ -108,107 +165,129 @@ int archiveFile(char *dir, FILE* file_out){
     fseek(file_to_archive, 0, SEEK_SET);
     char buffer[BUFFSIZE1024];
     size_t total_bytes_read = 0;
-    while((total_bytes_read = fread(buffer, 1, BUFFSIZE1024, file_to_archive)) > 0){
-        if (fwrite(buffer, 1, total_bytes_read, file_out)!= total_bytes_read){
+    while ((total_bytes_read = fread(buffer, 1, BUFFSIZE1024, file_to_archive)) > 0)
+    {
+        if (fwrite(buffer, 1, total_bytes_read, file_out) != total_bytes_read)
+        {
             fclose(file_to_archive);
             fclose(file_out);
-            return 3;
+            return ERR_WRT_ARCH;
         }
     }
-    
+
     fputs("\n", file_out);
     fclose(file_to_archive);
     return 0;
 }
 
-int processDir(char *dir, FILE* file_out) {
- 
- DIR *dp;
- struct dirent *entry;
- struct stat statbuf;
- int errCode = 0;
+int processDir(char *dir, FILE *file_out, size_t root_size)
+{
 
- if ((dp = opendir(dir)) == NULL) {
-  return 1;
- }
- chdir(dir);
- while((entry = readdir(dp)) != NULL) {
-  lstat(entry->d_name, &statbuf);
-  if (S_ISDIR(statbuf.st_mode)) {
-   /* Finds the directory but ignores . and .. */
-   if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
-    continue;
-    // Enters a new directory
-   if ((errCode = processDir(entry->d_name, file_out))!=0){
-    return errCode;
-   }
-  } else {
-    if ((errCode=archiveFile(entry->d_name, file_out))!=0) // Archiving
+    DIR *dp;
+    struct dirent *entry;
+    struct stat statbuf;
+    int errCode = 0;
+
+    if ((dp = opendir(dir)) == NULL)
     {
-        return errCode;
+        return ERR_OPN_DIR;
     }
-  }
- }
- chdir("..");
- closedir(dp);
- return 0;
+    chdir(dir);
+    while ((entry = readdir(dp)) != NULL)
+    {
+        lstat(entry->d_name, &statbuf);
+        if (S_ISDIR(statbuf.st_mode))
+        {
+            /* Finds the directory but ignores . and .. */
+            if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
+                continue;
+            // Enters a new directory
+            if ((errCode = processDir(entry->d_name, file_out, root_size)) != 0)
+            {
+                return errCode;
+            }
+        }
+        else
+        {
+            if ((errCode = archiveFile(entry->d_name, root_size, file_out)) != 0) // Archiving
+            {
+                return errCode;
+            }
+        }
+    }
+    chdir("..");
+    closedir(dp);
+    return 0;
 }
 
-int processArchive(FILE* archiveFile) {
+int processArchive(FILE *archiveFile, char *top_dir_path)
+{
     int errCode = 0;
     char buffer[BUFFSIZE1024];
 
-    while (fgets(buffer, BUFFSIZE1024, archiveFile)) {
+    while (fgets(buffer, BUFFSIZE1024, archiveFile))
+    {
         char original_filename[BUFFSIZE1024]; // Store the original path before createNewDirectory() called
-        
+
         char *filename = strtok(buffer, " | ");
         size_t size = atol(strtok(NULL, " | "));
-        strncpy(original_filename, filename, BUFFSIZE1024); //save the original path before createNewDirectory() called
 
         // Recover the directory structure
-        if ((errCode = createNewDirectory(strtok(filename, "/"), ""))!=0){
+        // filename = top_dir_path + filename
+        char *newPath = (char *)malloc(strlen(top_dir_path) + strlen(filename) + 1); // +1 for '/', +1 for '\0'
+        strcpy(newPath, top_dir_path);
+        strcat(newPath, filename);
+        strncpy(original_filename, newPath, BUFFSIZE1024); // save the original path before createNewDirectory() called
+
+        if ((errCode = createNewDirectory(strtok(newPath, "/"), "")) != 0)
+        {
             return errCode;
         }
 
         // Create a new file in the decompressed directory
-        FILE* file_to_write = fopen(original_filename, "wb");
-        if (!file_to_write) {
-            return 2;
+        FILE *file_to_write = fopen(original_filename, "wb");
+        if (!file_to_write)
+        {
+            return ERR_OPN_FILE;
         }
-        
+
         // Read the file data from the archive
-        char *data = (char*)malloc(size*sizeof(char)); // dynamical array
-        if (fread(data, 1, size, archiveFile) != size) {
+        char *data = (char *)malloc(size * sizeof(char)); // dynamical array
+        if (fread(data, 1, size, archiveFile) != size)
+        {
             fprintf(stderr, "Error reading file data for %s\n", original_filename);
             fclose(file_to_write);
             free(data);
-            return 4;
+            return ERR_RD_ARCH;
         }
 
         // Write the file data to the
-        if (fwrite(data, 1, size, file_to_write) != size) {
+        if (fwrite(data, 1, size, file_to_write) != size)
+        {
             fprintf(stderr, "Error writing data to %s\n", original_filename);
             fclose(file_to_write);
             free(data);
-            return 5;
+            return ERR_EXTR_ARCH;
         }
 
         // free allocated memory
         free(data);
+        free(newPath);
         fclose(file_to_write);
 
         // Skip to the next line in the archive
         fgets(buffer, BUFFSIZE1024, archiveFile);
-
     }
     return 0;
 }
 
 // Рекурсивная функция для создания директорий
-int createNewDirectory(char *curr_dir, char *base_path) {
+int createNewDirectory(char *curr_dir, char *base_path)
+{
     int errCode = 0;
 
-    if (curr_dir == NULL) {
+    if (curr_dir == NULL)
+    {
         return 0; // Если нет больше частей пути, выходим
     }
 
@@ -219,7 +298,8 @@ int createNewDirectory(char *curr_dir, char *base_path) {
     char current_path[BUFFSIZE1024];
     snprintf(current_path, BUFFSIZE1024, "%s/%s", base_path, curr_dir);
 
-    if (next_part == NULL) {
+    if (next_part == NULL)
+    {
         // Если это последняя часть пути, значит, это файл
         printf("File to create: %s\n", current_path); // Обработка файла в будущем
         return 0;
@@ -227,49 +307,59 @@ int createNewDirectory(char *curr_dir, char *base_path) {
 
     // Проверяем, существует ли директория
     struct stat statbuf;
-    if (stat(current_path, &statbuf) != 0) {
+    if (stat(current_path, &statbuf) != 0)
+    {
         // Директории не существует, создаем ее
-        if (mkdir(current_path, 0777) != 0) {
-            return 6;
+        if (mkdir(current_path, 0777) != 0)
+        {
+            return ERR_CRT_DIR;
         }
         printf("Created directory: %s\n", current_path);
-    } else if (!S_ISDIR(statbuf.st_mode)) {
+    }
+    else if (!S_ISDIR(statbuf.st_mode))
+    {
         // Существующий путь - не директория, ошибка
-        return 7;
+        return ERR_EXTR;
     }
 
     // Переходим к следующей части пути
-    if ((errCode=createNewDirectory(next_part, current_path))!=0){
+    if ((errCode = createNewDirectory(next_part, current_path)) != 0)
+    {
         return errCode;
     }
     return 0;
 }
 
-void PrintErr(int errorCode){
-    switch(errorCode){
-        case 1:
-            fprintf(stderr, "Error opening directory\n");
-            break;
-        case 2:
-            fprintf(stderr, "Error opening file\n");
-            break;
-        case 3:
-            fprintf(stderr, "Error writing to archive file\n");
-            break;
-        case 4:
+void PrintErr(int errorCode)
+{
+    switch (errorCode)
+    {
+    case ERR_OPN_DIR:
+        fprintf(stderr, "Error opening directory\n");
+        break;
+    case ERR_OPN_FILE:
+        fprintf(stderr, "Error opening file\n");
+        break;
+    case ERR_WRT_ARCH:
+        fprintf(stderr, "Error writing to archive file\n");
+        break;
+    case ERR_RD_ARCH:
         fprintf(stderr, "Error reading archived data\n");
         break;
-        case 5:
+    case ERR_EXTR_ARCH:
         fprintf(stderr, "Error extracting archived data\n");
         break;
-        case 6:
+    case ERR_CRT_DIR:
         fprintf(stderr, "Error creating directory\n");
         break;
-        case 7:
+    case ERR_EXTR:
         fprintf(stderr, "Error: not a directory requested to extract\n");
         break;
-        default:
-            fprintf(stderr, "Unknown error code: %d\n", errorCode);
-            break;
+    case ERR_PTH_ARCH:
+        fprintf(stderr, "Error of extraction: incorrect archive path\n");
+        break;
+    default:
+        fprintf(stderr, "Unknown error code: %d\n", errorCode);
+        break;
     }
 }
